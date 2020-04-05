1adc6b76dc360ed8e117a4d3daf2f6d5725930a5
sstejesh <63189490+sstejesh@users.noreply.github.com>
2020-04-05 17:07:44 +0530

    printpositivenum.py

---
+++ printpositivenum.py
@@ -0,0 +1,22 @@
+list1=[12,-7,5,64,-14]
+list2=[12,14,-95,3]
+a=len(list1)
+b=len(list2)
+i=0
+while i<=a:
+  if list1[i]>0:
+    output1.append(list1[i])
+  end  
+i+=1
+end
+print(op1)
+i=0
+while i<=b:
+  if list2[i]>0:
+    output2.append(list2[i])
+  end  
+i+=1
+end
+print(op2)
+
+  
