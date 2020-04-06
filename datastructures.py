38ea5d4d32e4aefae46f4820363e5d88ecaaf633
sstejesh <63189490+sstejesh@users.noreply.github.com>
2020-04-06 14:39:30 +0530

    datastructures.py

---
+++ datastructures.py
@@ -0,0 +1,29 @@
+list1=[1,2,3,4,5]
+list2=["apple","banana","grapes"]
+list3=['a','e','i','o','u']
+print("given lists are:")
+print(list1)
+print(list2)
+print(list3)
+list1.append(6)
+list2.append("mango")
+del list3[4]
+print("lists after changing:")
+print(list1)
+print(list2)
+print(list3)
+tuple=(1,2,3,4,5)
+print("length of tuple is:")
+print(len(tuple))
+print("each element of tuple is:")
+print(tuple[0])
+print(tuple[1])
+print(tuple[2])
+print(tuple[3])
+print(tuple[4])
+dict={"kohli":"batsman","dhoni":"captain","bumrah":"bowler}
+print("after deleting some elements")
+del dict(bumrah)
+del dict(dhoni)
+print(dict)
+
